c985cdae98f4dcd251663c7faca9c59a0b389664
Julian Rommel <julian.rommel@posteo.de>
2024-08-29 20:16:08 +0200

    input is now one equation and operator will be found dynamically

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,63 @@
 use std::io;
 
 fn main() {
-    let mut input_1 = String::new();
-    let mut input_2 = String::new();
+    let possible_operations = ["*", "+", "-"];
 
-    println!("Enter first number");
+    let mut input_1 = String::new();
+    // let mut input_2 = String::new();
+
+    println!("Enter equation");
+
     io::stdin()
         .read_line(&mut input_1)
         .expect("Failed to read number!");
 
-    println!("Enter second number");
-    io::stdin()
-        .read_line(&mut input_2)
-        .expect("Failed to read number!");
+    // let number_1: i32 = input_1
+    // .trim()
+    // .parse::<i32>()
+    // .expect("Not a valid Number 1!");
+    for operation in possible_operations {
+        println!("try {operation}");
+        let splitted: Vec<&str> = input_1.trim().split(operation).collect();
+        // println!("{splitted}");
+        // let splitted_option: Option<Vec<&str>> = splitted.collect();
+        // for element in splitted {
+        // assert_eq!(element, None);
+        println!("{:?}", splitted);
 
-    let number_1: i32 = input_1
-        .trim()
-        .parse::<i32>()
-        .expect("Not a valid Number 1!");
-    let number_2: i32 = input_2
-        .trim()
-        .parse::<i32>()
-        .expect("Not a valid Number 2!");
-    // println!("{number_1}");
-    let operation = String::from("+");
+        if splitted.len() > 1 {
+            let calculated_result = calculate(
+                splitted[0].parse::<i32>().unwrap(),
+                splitted[1].parse::<i32>().unwrap(),
+                operation,
+            );
 
-    let result = calculate(number_1, number_2, operation);
-    println!("{}", result);
+            println!("{calculated_result}");
+            break;
+        }
+
+        // }
+        // println!("{splitted_option}");
+        // }
+    }
+
+    // Raise error as now correct equation has been found
+    println!("No correct equation has been found, try again!");
+
+    // let operation = String::from("+");
+
+    // let result = calculate(number_1, number_2, operation);
+    // println!("{}", result);
     // println!("Number1: {}; Number2: {}", input_1, input_2);
 }
 
-fn calculate(x: i32, y: i32, operation: String) -> i32 {
-    match operation.as_str() {
-        "+" => return x + y,
-        "-" => return x - y,
-        "*" => return x * y,
-        "/" => return x / y,
-        _ => return 0,
-    };
+fn calculate(x: i32, y: i32, operation: &str) -> i32 {
+    match operation {
+        "+" => x + y,
+        "-" => x - y,
+        "*" => x * y,
+        "/" => x / y,
+        _ => 0,
+    }
     // return x + y;
 }
